842580c8462a07abf859c38102154ed6407684a8
MAHESHWARAN <maheshwaran@eam360.com>
2024-01-27 15:40:38 +0530

    first commit

---
+++ ExampleUI/Views/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ExampleUI
+//
+//  Created by MAHESHWARAN on 27/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupView()
+  }
+  
+  private func setupView() {
+    title = "Example"
+    view.backgroundColor = .systemBackground
+  }
+}
+
